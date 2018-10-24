1e6277749585c7deb388424bba278f2756c5117e
andrea-longo <longoandrea1996@gmail.com>
2018-10-24 23:31:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 * © 2015-2018 Kasra Madadipouya <kasra@madadipouya.com>
 *
 */
+    
 require 'vendor/autoload.php';
 
 $client = new Zelenin\Telegram\Bot\Api('524326842:AAHKIV8D06niOlDE84sOR0k9DitpGN-INFA');
@@ -28,17 +29,52 @@ $cit = array(
     "Maleducata troia dea Madonna."
 );
 
+
+//your app
 try {
 
-    if($update->message->text == '/citazione')
+    if($update->message->text == '/email')
     {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
         	'chat_id' => $update->message->chat->id,
-        	'text' => $cit[0];
+        	'text' => "You can send email to : Kasra@madadipouya.com"
      	]);
     }
+    else if($update->message->text == '/help')
+    {
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => $cit[1] . "Hello"
+    		]);
 
+    }
+    else if($update->message->text == '/latest')
+    {
+    		Feed::$cacheDir 	= __DIR__ . '/cache';
+			Feed::$cacheExpire 	= '5 hours';
+			$rss 		= Feed::loadRss($url);
+			$items 		= $rss->item;
+			$lastitem 	= $items[0];
+			$lastlink 	= $lastitem->link;
+			$lasttitle 	= $lastitem->title;
+			$message = $lasttitle . " \n ". $lastlink;
+			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+			$response = $client->sendMessage([
+					'chat_id' => $update->message->chat->id,
+					'text' => $message
+				]);
+
+    }
+    else
+    {
+    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+    		'chat_id' => $update->message->chat->id,
+    		'text' => "Invalid command, please use /help to get list of available commands"
+    		]);
+    }
 
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
 
