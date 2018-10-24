ac316c8238d96fcf10d2f6723c857dea51b8a91a
Andrea Longo <longoandrea1996@gmail.com>
2018-10-24 22:36:58 +0200

    Commit

--- index.php
+++ index.php
@@ -1,78 +1,74 @@
-<?php
+﻿<?php
+//è necessario aggiungere bot prima del nostro token
+//You must add bot before our token
+$botToken = "bot"."524326842:AAHKIV8D06niOlDE84sOR0k9DitpGN-INFA";
 
-/*
-* This file is part of GeeksWeb Bot (GWB).
-*
-* GeeksWeb Bot (GWB) is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 3
-* as published by the Free Software Foundation.
-* 
-* GeeksWeb Bot (GWB) is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
-*
-* Author(s):
-*
-* © 2015-2018 Kasra Madadipouya <kasra@madadipouya.com>
-*
-*/
-require 'vendor/autoload.php';
+//Recuperiamo l'input che riceveremo dal bot
+//We retrieve the input we receive from bot
+$TelegramRawInput = file_get_contents("php://input");
+// php://input restituisce i dati raw (testo), andremo quindi a formattare il tutto in un array, i dati che riceveremo saranno in formato Json quindi un json_decode farà al caso nostro.
+// php // input returns the raw data (text), then we will format everything in an array, the data we receive will be in Json format so a json_decode will be for us.
+$update = json_decode($TelegramRawInput, TRUE);
 
-$client = new Zelenin\Telegram\Bot\Api('524326842:AAHKIV8D06niOlDE84sOR0k9DitpGN-INFA'); // Set your access token
-$url = ''; // URL RSS feed
-$update = json_decode(file_get_contents('php://input'));
-
-//your app
-try {
-
-    if($update->message->text == '/email')
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-        	'chat_id' => $update->message->chat->id,
-        	'text' => "You can send email to : Kasra@madadipouya.com"
-     	]);
-    }
-    else if($update->message->text == '/help')
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "List of commands :\n /email -> Get email address of the owner \n /latest -> Get latest posts of the blog 
-    		/help -> Shows list of available commands"
-    		]);
-
-    }
-    else if($update->message->text == '/latest')
-    {
-    		Feed::$cacheDir 	= __DIR__ . '/cache';
-			Feed::$cacheExpire 	= '5 hours';
-			$rss 		= Feed::loadRss($url);
-			$items 		= $rss->item;
-			$lastitem 	= $items[0];
-			$lastlink 	= $lastitem->link;
-			$lasttitle 	= $lastitem->title;
-			$message = $lasttitle . " \n ". $lastlink;
-			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-			$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => $message
-				]);
-
-    }
-    else
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "Invalid command, please use /help to get list of available commands"
-    		]);
-    }
-
-} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
-
-    //echo error message ot log it
-    //echo $e->getMessage();
-
+//Assicuriamoci di aver ricevuto un update, altrimenti interrompiamo l'esecuzione
+//Make sure you have received an update, otherwise we interrupt execute
+if(!$update)
+{
+  exit;
 }
+
+//Recuperiamo l'oggetto message dal json
+//We recover the message object from json
+$MessageObj = $update['message'];
+//Recuperiamo il chatId, che utilizzeremo per rispondere all'utente che ci ha appena invocato
+//We recover the chatId table which we will use to respond to the user who has just invoked
+$chatId = $MessageObj['chat']['id'];
+
+//Salvo il json ricevuto per analizzarlo in seguito
+//We save the json received to parse it later
+saveInJsonFile($update, "ricevuto.json");
+
+//Rispondiamo HelloWorld
+//We answer HelloWorld
+$out = sendMsg($botToken,$chatId,"Hello World!");
+
+//Salvo il json ricevuto per analizzarlo in seguito
+//We save the json received to parse it later
+saveInJsonFile($out, "inviato.json");
+
+
+/**
+ * 
+ * FUNCTION AREA
+ * 
+ */
+
+//Funzione per far inviare un messaggio da parte del bot
+//Function to send a message from the bot
+function sendMsg($tkn, $cId, $msgTxt){
+    /*
+        Creiamo la URL per richiamare la API Telegram apposita, nel nostro caso sarà la sendMessage.
+        Questa API richiede due parametri obbligatori, chatId e Testo del messaggio
+        NB: La chiamata alla API sarà in GET, quindi è consigliato (fortemente consigliato) di inviare il testo all'interno di un urlencode().
+
+
+        Create the URL to invoke the appropriate API in this case will be the Telegram sendMessage. 
+        This API requires two required parameters, chatId and message text 
+        NOTE: the call to the API will GET, so it is recommended (strongly recommended) to send the text within a urlencode ().
+    */
+    $TelegramUrlSendMessage = "https://api.telegram.org/".$tkn."/sendMessage?chat_id=".$cId."&text=".urlencode($msgTxt);
+    
+    //Come return della funzione restituiremo l'output di file_get_contents della URL appena creata.
+    //As a return of the function we will return the output of file_get_contents of the URL just created.
+    return file_get_contents($TelegramUrlSendMessage);
+}
+
+//Questa è la funzione che utilizzo per salvare il json nei file
+//This is the function i use to save the json in file
+function saveInJsonFile($data, $filename){
+    if(file_exists($filename))
+        unlink($filename);
+    file_put_contents($filename,json_encode($data,JSON_PRETTY_PRINT));
+}
+
+?>
