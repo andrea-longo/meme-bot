b31bc49129a139081494f4e8cb87beb4ff3b3bfe
Andrea Longo <longoandrea1996@gmail.com>
2018-10-24 23:19:17 +0200

    some fix

--- index.php
+++ index.php
@@ -19,56 +19,26 @@
 */
 require 'vendor/autoload.php';
 
-$client = new Zelenin\Telegram\Bot\Api('524326842:AAHKIV8D06niOlDE84sOR0k9DitpGN-INFA'); // Set your access token
+$client = new Zelenin\Telegram\Bot\Api('524326842:AAHKIV8D06niOlDE84sOR0k9DitpGN-INFA');
 $url = ''; // URL RSS feed
 $update = json_decode(file_get_contents('php://input'));
 
-//your app
+$cit = array(
+    "15 e 18 quanto fa coglione?",
+    "Maleducata troia dea Madonna."
+);
+
 try {
 
-    if($update->message->text == '/email')
+    if($update->message->text == '/citazione')
     {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
         	'chat_id' => $update->message->chat->id,
-        	'text' => "You can send email to : Kasra@madadipouya.com"
+        	'text' => $cit[array_rand($cit, 1)[0]];
      	]);
     }
-    else if($update->message->text == '/help')
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "List of commands :\n /email -> Get email address of the owner \n /latest -> Get latest posts of the blog 
-    		/help -> Shows list of available commands"
-    		]);
 
-    }
-    else if($update->message->text == '/latest')
-    {
-    		Feed::$cacheDir 	= __DIR__ . '/cache';
-			Feed::$cacheExpire 	= '5 hours';
-			$rss 		= Feed::loadRss($url);
-			$items 		= $rss->item;
-			$lastitem 	= $items[0];
-			$lastlink 	= $lastitem->link;
-			$lasttitle 	= $lastitem->title;
-			$message = $lasttitle . " \n ". $lastlink;
-			$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-			$response = $client->sendMessage([
-					'chat_id' => $update->message->chat->id,
-					'text' => $message
-				]);
-
-    }
-    else
-    {
-    	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-    	$response = $client->sendMessage([
-    		'chat_id' => $update->message->chat->id,
-    		'text' => "Invalid command, please use /help to get list of available commands"
-    		]);
-    }
 
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
 
