49563bb66f538fc2f50fdbca888ea33fbc6e4564
Andrea Longo <longoandrea1996@gmail.com>
2018-11-02 14:06:19 +0100

    Commit

--- index.php
+++ index.php
@@ -155,7 +155,7 @@ try {
 		$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
 			'chat_id' => $update->message->chat->id,
-			'text' => $user
+			'text' => $user. " " . $offese[array_rand($offese,1)]
 		]);
 	}
 		
