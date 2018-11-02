28c21e433ee905e429a77c10f687703018711a8c
Andrea Longo <longoandrea1996@gmail.com>
2018-11-02 13:11:03 +0100

    Commit

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 		"Paghei a multa i marochini Dio can",
 		"Xe ea parte mia chea là",
 		"Me toca infetarlo de biasteme",
-		"Dio bifolco, trifolco, superfolco",
+		"Ma varda ti quanto che l'è bifolco, trifolco, superfolco el Cristo Dio porco",
 		"Dove xeo el classon su sto merda de camion Dio bastardo",
 		"Dio bestia",
 		"Pronto per ripartire?",
@@ -47,7 +47,16 @@
 		"So pí veoce mi de ti",
 		"Ea fiera dee tonelate dea merda",
 		"Dio schifoso cane",
-		"Chi è quel mona che sbate le porte"
+		"Chi è quel mona che sbate le porte",
+		"Perchè signore no go la possibilità de smaterializzarme",
+		"Sto tornando merde",
+		"Oggi ho vinto 30 euro alle macchinette",
+		"Po...Po...Porco Dio, Popopo porca Madonna",
+		"Dio malato Dio cane",
+		"Altro che Maria Vergine, Maria Vacca",
+		"Madonna tripla",
+		"Orca timonea"
+
 	);
 
 try {
@@ -56,15 +65,15 @@ try {
     	$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
     	$response = $client->sendMessage([
     		'chat_id' => $update->message->chat->id,
-    		'text' => "I comandi disponibili sono:
-			\nCit - Fornisce una citazione casuale
-			\nVideo palazzo pd - palazzo porco dio
-			\nVideo pompino - pompino con sparo
-			\nVideo sberla - manifestazione contro i bambini
-			\nVideo zia gabriella - tanti auguri zia Gabriella
-			\nVideo 15 e 18 - 15 e 18 quanto fa coglione?
-			\nVideo di merdah - panzone affannato
-			\nVideo il motore - barca perde il motore"
+    		'text' => "I comandi disponibili sono:"
+			. "\nCit - Fornisce una citazione casuale"
+			. "\nVideo palazzo pd - palazzo porco dio"
+			."\nVideo pompino - pompino con sparo"
+			."\nVideo sberla - manifestazione contro i bambini"
+			."\nVideo zia gabriella - tanti auguri zia Gabriella"
+			."\nVideo 15 e 18 - 15 e 18 quanto fa coglione?"
+			."\nVideo di merdah - panzone affannato"
+			."\nVideo il motore - barca perde il motore"
     	]);
     }
     else if($update->message->text == 'cit' || $update->message->text == 'Cit') {
@@ -122,7 +131,15 @@ try {
 			'chat_id' => $update->message->chat->id,
 			'text' => 'https://youtu.be/36PXfNdYLGk'
 		]);
-    }		  
+    }
+	else if(strpos($update->message->text, 'Offendi')){
+		$user = str_replace("Offendi", "", $update->message->text);
+		$response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
+    	$response = $client->sendMessage([
+			'chat_id' => $update->message->chat->id,
+			'text' => $user . "Putana to mare, bifolco to pare."
+		]);
+	}
 		
 } catch (\Zelenin\Telegram\Bot\NotOkException $e) {
 
